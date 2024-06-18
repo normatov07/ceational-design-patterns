73554a0cce3ce6d59a73d12cbc8fe68f24225574
normatov07 <normatov.milliy@gmail.com>
2024-06-19 00:57:21 +0500

    add php implementation

---
+++ factory-method/factory-method.php
@@ -0,0 +1,54 @@
+<?php
+
+interface DB
+{
+    function connect(): void;
+    function execute();
+}
+
+class Mysql implements DB
+{
+    function connect(): void
+    {
+    }
+
+    function execute()
+    {
+    }
+}
+
+interface DBFactory
+{
+    function createDB(): DB;
+}
+
+class MysqlFactory implements DBFactory
+{
+    function createDB(): DB
+    {
+        return new Mysql;
+    }
+}
+
+
+class Client
+{
+
+    protected DB $db;
+
+    public function __construct()
+    {
+        $this->getDB(new MysqlFactory);
+    }
+
+    function getDB(DBFactory $factory)
+    {
+        $this->db = $factory->createDB();
+        $this->db->connect();
+    }
+
+    function operation(): void
+    {
+        $this->db->execute();
+    }
+}

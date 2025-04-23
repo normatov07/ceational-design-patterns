5b2655fd43dd2c204437ba38e21854ffe7427389
normatov07 <normatov.milliy@gmail.com>
2025-04-23 18:48:03 +0500

    add golang singleton implemenation

--- singleton/singleton.go
+++ singleton/singleton.go
@@ -1,1 +1,30 @@
 package main
+
+import "sync"
+
+var mutex = sync.Mutex{}
+
+type Product struct{}
+
+func (p *Product) DoSomething() {}
+
+var productInstance *Product
+
+func GetProductInstance() *Product {
+	if productInstance == nil {
+		mutex.Lock()
+		defer mutex.Unlock()
+		if productInstance == nil {
+			productInstance = &Product{}
+		}
+	}
+
+	return productInstance
+}
+
+// Client implementation
+func main() {
+	product := GetProductInstance()
+
+	product.DoSomething()
+}
